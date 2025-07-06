197104343648497d191a3beeda6d5484ad313493
INHO <kiminho00@Naver.com>
2025-07-06 21:10:55 +0900

    change region

--- lambda_build.sh
+++ lambda_build.sh
@@ -4,9 +4,9 @@ docker build -t lambda-container-example . --no-cache
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
-docker tag lambda-container-example $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/lambda-container-example
+docker tag lambda-container-example $ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com/lambda-container-example
 
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com
 
 docker push $ACCOUNT_ID.dkr.ecr.ap-northeast-2.amazonaws.com/lambda-container-example
 
